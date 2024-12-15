898da2685c353c6a7e1504fe2ea06d56c9a2da1b
Limerence <114558251+yeteye@users.noreply.github.com>
2024-12-15 16:26:32 +0800

    后端

---
+++ db_work_back/src/main/java/org/example/db_work_back/DbWorkBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.db_work_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbWorkBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbWorkBackApplication.class, args);
+    }
+
+}
